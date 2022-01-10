55a0e9bcc8591673733241b4baae005ad8966137
向山　翔 <hms0175km@gmail.com>
2022-01-10 12:13:11 +0900

    [Add]Promise使用

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-setTimeout(() => {
-    console.log(3);
-    setTimeout(() => {
-        console.log(2);
-        setTimeout(() => {
-            console.log(1)
-        }, 1000);
-    }, 1000);
-}, 1000);
+// setTimeout(() => {
+//     console.log(3);
+//     setTimeout(() => {
+//         console.log(2);
+//         setTimeout(() => {
+//             console.log(1)
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
 
+new Promise(resolve => {
+    setTimeout(() => {
+        console.log(3);
+        resolve();
+    },1000)
+}).then(() => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(2);
+            resolve();
+        }, 1000)
+    });
+}).then(() => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(1);
+            resolve();
+        }, 1000)
+    });
+})
