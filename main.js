62e3201baa6722c3f62bbe7af5ba41f6e79c2fc8
向山　翔 <hms0175km@gmail.com>
2022-01-10 12:03:38 +0900

    [Add]プロミスなし

---
+++ main.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    console.log(3);
+    setTimeout(() => {
+        console.log(2);
+        setTimeout(() => {
+            console.log(1)
+        }, 1000);
+    }, 1000);
+}, 1000);
+
