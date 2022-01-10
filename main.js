aea64b6744f8fa0e8999a50df8304d8dcc82e981
向山　翔 <hms0175km@gmail.com>
2022-01-10 12:23:28 +0900

    [Finish]async await

--- main.js
+++ main.js
@@ -8,23 +8,47 @@
 //     }, 1000);
 // }, 1000);
 
-new Promise(resolve => {
-    setTimeout(() => {
-        console.log(3);
-        resolve();
-    },1000)
-}).then(() => {
+
+// Promise
+
+
+// new Promise(resolve => {
+//     setTimeout(() => {
+//         console.log(3);
+//         resolve();
+//     },1000)
+// }).then(() => {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             console.log(2);
+//             resolve();
+//         }, 1000)
+//     });
+// }).then(() => {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             console.log(1);
+//             resolve();
+//         }, 1000)
+//     });
+// })
+
+
+
+// async使用
+
+func = async () => {//関数に対してasyncと宣言
+    await log(3);
+    await log(2);   //「この関数は非同期関数」
+    await log(1);   //関数の呼び出し前にawait
+};//⇨Promiseの結果が帰ってくるまで待つ
+
+log = (num) => {//Promiseを返す関数
     return new Promise(resolve => {
         setTimeout(() => {
-            console.log(2);
+            console.log(num);
             resolve();
-        }, 1000)
-    });
-}).then(() => {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            console.log(1);
-            resolve();
-        }, 1000)
-    });
-})
+        },1000)
+    })
+}
+func()
